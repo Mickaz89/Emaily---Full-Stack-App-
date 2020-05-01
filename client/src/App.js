f6afaefc3585ac40aa9da549c9f12e43f95aedb6
Mickael <zanamickael@gmail.com>
2020-05-01 16:31:09 +0300

    Fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Router , Route, Switch, withRouter} from "react-router-dom";
+import {Router , Route, Switch, withRouter, Redirect} from "react-router-dom";
 import {connect} from "react-redux";
 import Login from "./components/Login/Login";
 import PrivateRoute from './modules/routes/PrivateRoute'
@@ -22,6 +22,7 @@ class App extends React.Component  {
     }
             return (
                 <Switch>
+                    <Route exact path="/" render={() => <Redirect to={this.props.auth ? '/dashboard' : '/login'}/>}/>
                     <PrivateRoute exact path="/dashboard" ><DashboardLayout/></PrivateRoute>
                     <Route exact path="/login" component={Login} />
                 </Switch>
